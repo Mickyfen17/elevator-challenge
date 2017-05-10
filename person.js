1777362a0ab319a1868389ebdcaae0f331e055ff
Michael Fenwick <mickyfen17@aol.com>
2017-05-09 19:31:35 -0600

    add rider request time to person class

--- person.js
+++ person.js
@@ -1,9 +1,8 @@
 export default class Person {
-  constructor({ name, currentFloor, dropOffFloor }) {
+  constructor({ name, currentFloor, dropOffFloor, time }) {
     this.name = name || '';
     this.currentFloor = currentFloor || null;
     this.dropOffFloor = dropOffFloor || null;
+    this.requestTime = time || '5:00pm';
   }
-
-
 }
